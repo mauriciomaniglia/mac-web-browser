84330c257428f1e6c6dfa361ebb4ae8865ee898d
mauriciomaniglia <manigliamauricio@gmail.com>
2023-01-10 21:20:42 -0300

    Remove unused methods declaration in AppDelegate.

--- mac-web-browser/mac-web-browser/AppDelegate.swift
+++ mac-web-browser/mac-web-browser/AppDelegate.swift
@@ -7,13 +7,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         MainComposer.composerWebView()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
 }
 
