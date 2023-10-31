17aadbd2d5d912ba35f0bad0ca08356dd09df100
mauriciomaniglia <manigliamauricio@gmail.com>
2023-10-31 07:51:07 -0300

    Decouple WebKit from WindowView

--- mac-web-browser/mac-web-browser/Window/Composition/WindowComposer.swift
+++ mac-web-browser/mac-web-browser/Window/Composition/WindowComposer.swift
@@ -4,10 +4,10 @@ import core_web_browser
 final class WindowComposer {
     static func composerWebView() {
         let windowController = NSApplication.shared.windows.first?.windowController as! WindowController
-        let view = WindowView()
-        let webViewProxy = WebViewProxy(webView: view.webView)
+        let webViewProxy = WebViewProxy()
+        let view = WindowView(webView: webViewProxy.webView)
         let windowPresenter = WindowPresenter()
-        let windowViewAdapter = WindowViewAdapter(webViewProxy: webViewProxy, presenter: windowPresenter)
+        let windowViewAdapter = WindowViewAdapter(webView: webViewProxy, presenter: windowPresenter)
 
         windowController.contentViewController?.view = view
         webViewProxy.delegate = windowViewAdapter
